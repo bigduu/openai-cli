f5de6ca6e037297259a08c0bc4f947b6cd2295b9
bigduu <mugeng.du@qq.com>
2023-02-15 00:03:49 +0800

    chagne to command line

--- src/main.rs
+++ src/main.rs
@@ -12,23 +12,24 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     let openai_info = OpenAIInfo::new("application.yml".to_string())?;
     let open_client = OpenAIClient::new(openai_info.get_token(), openai_info.base_url());
-    let mut input = String::new();
-    println!("You can input 'quit' to exit");
-    println!("Please input your question:");
-    loop {
-        std::io::stdin().read_line(&mut input)?;
-        if "quit" == input.trim() {
-            break;
-        }
-        let ans = open_client.query(input.clone()).await?;
-        let lines = ans.choices[0].text();
-        lines.iter().for_each(|line| println!("{}", line));
-        println!("----------------------------------------");
-        println!("finish_reason:{},", ans.choices[0].finish_reason);
-        println!("You can input continue or next question");
-        println!("----------------------------------------");
-        input.clear();
+    let mut args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Please input your question");
+        return Ok(());
     }
+    args.remove(0);
+    let mut query = String::new();
+    for ele in args.into_iter() {
+        query.push_str(&ele);
+        query.push(' ');
+    }
+    println!("Query question:\n [{}] \nplease wait...", query.clone());
+    let ans = open_client.query(query).await?;
+    let lines = ans.choices[0].text();
+    lines.iter().for_each(|line| println!("{}", line));
+    println!("----------------------------------------");
+    println!("finish_reason:{},", ans.choices[0].finish_reason);
+    println!("----------------------------------------");
 
     Ok(())
 }
